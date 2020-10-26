a07d006f060adb4ed0598fe4d4e6dde983b3afec
TSKraak <tskraak@gmail.com>
2020-10-26 17:14:06 +0100

    Added items to todoItems using .create

--- seeders/20201026112108-some-todoLists.js
+++ seeders/20201026112108-some-todoLists.js
@@ -16,6 +16,6 @@ module.exports = {
   },
 
   down: async (queryInterface, Sequelize) => {
-    await queryInterface.bulkDelete("users", null, {});
+    await queryInterface.bulkDelete("todoLists", null, {});
   },
 };
